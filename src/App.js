76a0734c2b38cd42b9fc1a3d7b869a7a45d734c4
lollipopwyn <rwewine@gmail.com>
2024-12-31 15:23:27 +0900

    241231_chore_main_app파일 수정_나라_KAN-38

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React, { useState } from 'react';
-import ChatPage from './components/ChatPage'; // 채팅방 목록 컴포넌트
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './components/main/HomePage';
+import Sidebar from './components/common/Sidebar';
 
 function App() {
   return (
-    <div className="chat-container">
-      <ChatPage />
-    </div>
+    <BrowserRouter>
+      <div className="fix-section">
+        <Sidebar />
+      </div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
