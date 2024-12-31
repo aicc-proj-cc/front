23874fe5f7ab12963131936e0c6aa46ecc0df3fd
lollipopwyn <rwewine@gmail.com>
2024-12-31 15:05:58 +0900

    241231_chore_main_메인 페이지에서 푸터와 상단 바 제거,_나라_KAN-38

--- src/components/main/HomePage.jsx
+++ src/components/main/HomePage.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
-import SideBar from '../common/Sidebar';
-import Upperbar from '../common/UpperBar';
+
 import FiledSlider from './FiledSlider';
 import Section from './Section';
-import Footer from '../common/Footer';
 
 function HomePage() {
   return (
     <div>
       <FiledSlider />
       <Section />
-      <Footer />
     </div>
   );
 }
