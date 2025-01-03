5c6670ab64771681d9058866c07d7fdbd9de8c23
leejh7778 <chantforthehope@gmail.com>
2025-01-03 15:38:19 +0900

    채팅방 응답중 css 적용 및 대화요약 페이지(추가작업 필요)

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import CharacterSearch from './components/CharacterSearch'; // 캐릭터 조회 
 import ChatPage from './components/ChatPage'; // 채팅 페이지
 import AITestPage from './components/AITestPage'; // 이미지 생성 페이지
 import TTSPage from './components/TTSPage'; // TTS 테스트 페이지
-
+import ChatSummary from './components/ChatSummary';
 
 import './App.css';
 
@@ -20,17 +20,25 @@ function App() {
         <div className="app-main-content">
           <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path="/CharacterManager" element={<CharacterManager />} /> {/* 캐릭터 등록 */}
-            <Route path="/CharacterSearch" element={<CharacterSearch />} /> {/* 캐릭터 조회 */}
-            <Route path="/ChatPage" element={<ChatPage />} /> {/* 채팅 페이지 */}
-            <Route path="/AITestPage" element={<AITestPage />} /> {/* 이미지 생성 */}
+            <Route
+              path="/CharacterManager"
+              element={<CharacterManager />}
+            />{' '}
+            {/* 캐릭터 등록 */}
+            <Route path="/CharacterSearch" element={<CharacterSearch />} />{' '}
+            {/* 캐릭터 조회 */}
+            <Route path="/ChatPage" element={<ChatPage />} />{' '}
+            {/* 채팅 페이지 */}
+            <Route path="/AITestPage" element={<AITestPage />} />{' '}
+            {/* 이미지 생성 */}
             <Route path="/TTSPage" element={<TTSPage />} /> {/* TTS 테스트 */}
+            <Route path="/ChatSummary" element={<ChatSummary />} />{' '}
+            {/* 대화내용 요약 */}
           </Routes>
         </div>
       </div>
     </BrowserRouter>
   );
 }
-
 
 export default App;
