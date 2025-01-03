5253863f990133255d65fefe738bb5b29848494b
lollipopwyn <rwewine@gmail.com>
2025-01-03 17:41:48 +0900

    250103_chore_CREATE-IMAGE_이미지 생성 컴포넌트 이름 수정_나라_KAN-35

--- src/App.js
+++ src/App.js
@@ -4,9 +4,8 @@ import Sidebar from './components/common/Sidebar';
 import CharacterManager from './components/CharacterManager'; // 캐릭터 등록 페이지
 import CharacterSearch from './components/CharacterSearch'; // 캐릭터 조회 페이지
 import ChatPage from './components/ChatPage'; // 채팅 페이지
-import AITestPage from './components/AITestPage'; // 이미지 생성 페이지
+import ImageCreate from './components/ImageCreate'; // 이미지 생성 페이지
 import TTSPage from './components/TTSPage'; // TTS 테스트 페이지
-import ChatSummary from './components/ChatSummary';
 
 import './App.css';
 
@@ -29,11 +28,9 @@ function App() {
             {/* 캐릭터 조회 */}
             <Route path="/ChatPage" element={<ChatPage />} />{' '}
             {/* 채팅 페이지 */}
-            <Route path="/image-create" element={<AITestPage />} />{' '}
+            <Route path="/image-create" element={<ImageCreate />} />{' '}
             {/* 이미지 생성 */}
             <Route path="/TTSPage" element={<TTSPage />} /> {/* TTS 테스트 */}
-            <Route path="/ChatSummary" element={<ChatSummary />} />{' '}
-            {/* 대화내용 요약 */}
           </Routes>
         </div>
       </div>
